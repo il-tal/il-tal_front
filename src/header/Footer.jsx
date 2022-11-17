95d9b57dceaeb24e2218cdf2175f4688d3b24843
Hwirin-Kim <kimhuirin@gmail.com>
2022-11-17 14:46:52 +0900

    Chore : 작업 중간저장

---
+++ src/header/Footer.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return <Container />;
+};
+export default Footer;
+
+const Container = styled.footer`
+  height: 50px;
+  width: 100%;
+
+  background-color: black;
+`;
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,16 +5,31 @@ const Header = () => {
   const navigater = useNavigate();
   return (
     <Container>
-      <div onClick={() => navigater("/")}>집으로</div>
-      <div> / </div>
-      <div onClick={() => navigater("/test")}>실험실로</div>
+      <HeaderWrap>
+        <div onClick={() => navigater("/")}>집으로</div>
+        <div> / </div>
+        <div onClick={() => navigater("/test")}>실험실로</div>
+      </HeaderWrap>
     </Container>
   );
 };
 export default Header;
 
 const Container = styled.div`
-  height: 100%;
-  width: 100%;
+  height: 50px;
+  width: 100vw;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid gray;
+  position: sticky;
+  top: 0;
+  background-color: white;
+`;
+const HeaderWrap = styled.div`
+  height: 100%;
+  width: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
