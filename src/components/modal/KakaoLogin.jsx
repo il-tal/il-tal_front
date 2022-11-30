30ab287c1abee18c8ff5bdc28cf4db96c39f2166
Hwirin-Kim <kimhuirin@gmail.com>
2022-12-01 02:45:41 +0900

    Feature : 소셜로그인 구현 완료

--- src/components/modal/KakaoLogin.jsx
+++ src/components/modal/KakaoLogin.jsx
@@ -1,14 +1,25 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate, useLocation } from "react-router-dom";
+import { kakaologin2 } from "../../api";
 
 const KakaoLogin = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const KAKAO_CODE = location.search.split.split("=")[1];
+  const KAKAO_CODE = location.search.split("=")[1];
 
-  const getKakaoToken = () => {
-    fetch(`https://kauth.kakao.com/oauth/token`);
-  };
+  const { data } = useQuery(["getkakaotoken"], () => kakaologin2(KAKAO_CODE), {
+    onSuccess: (res) => {
+      sessionStorage.setItem("access_token", res.headers.access_token);
+      sessionStorage.setItem("refresh_token", res.headers.refresh_token);
+      sessionStorage.setItem("userinfo", JSON.stringify(res.data));
+      navigate("/");
+    },
+    onError: (err) => {
+      alert("로그인에 실패했습니다.");
+    },
+  });
+
+  return <div>Login...</div>;
 };
 
 export default KakaoLogin;
